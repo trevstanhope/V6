aad5e08a98805f712940aa002eb6f88a44a2b778
Trevor Stanhope <tpstanhope@gmail.com>
2015-01-30 00:12:37 -0500

    first

---
+++ V6.py
@@ -0,0 +1,147 @@
+import cv2, cv
+import numpy as np
+import time
+
+class V6:
+    
+    """
+    """
+    def __init__(self, cam_index=0, fov=60, dist=100, heading=0, incl=0, hessian=500, frame_w=640, frame_h=480, neighbors=2):
+        self.camera = cv2.VideoCapture(cam_index)
+        self.set_resolution(frame_w, frame_h)
+        self.set_fov(fov)
+        self.set_heading(heading)
+        self.set_inclination(incl) # 0 rad
+        self.set_distance(dist) # camera distance at center
+        self.set_neighbors(neighbors)
+        self.surf = cv2.SURF(hessian)
+        self.matcher = cv2.BFMatcher()
+    
+    """
+    """  
+    def close(self):
+        self.camera.release()
+    
+    """
+    Set Neighbors
+    """
+    def set_neighbors(self, neighbors):
+        if neighbors < 2:
+            raise Exception("Cannot have neighbors less than 2")
+        else:
+            self.neighbors = neighbors
+            
+    """
+    Set Resolution
+    frame_w [px]
+    frame_h [px]
+    """
+    def set_resolution(self, frame_w, frame_h):
+        if (frame_w <= 0) or (frame_h <= 0):
+            raise Exception("Improper frame size")
+        else:
+            self.frame_w = frame_w
+            self.frame_h = frame_h
+            self.camera.set(cv.CV_CAP_PROP_FRAME_WIDTH, frame_w)
+            self.camera.set(cv.CV_CAP_PROP_FRAME_HEIGHT, frame_h)
+    
+    """
+    Set distance at center of frame
+    dist [cm]
+    """
+    def set_distance(self, dist):
+        if dist <= 0:
+            raise Exception("Improper distance")
+        else:
+            self.dist = dist
+    
+    """
+    Set Inclination
+    incl [rad]
+    0 is orthogonal to surface 
+    """
+    def set_inclination(self, incl):
+        if incl < 0:
+            raise Exception("Cannot have negative inclination")
+        elif incl > np.pi/2.0:
+            raise Exception("Cannot have inclination parallel to surface")
+        else:
+            self.incl = incl
+    
+    """
+    Set Field-of-View
+    fov [rad]
+    """
+    def set_fov(self, fov):
+        if fov <= 0:
+            raise Exception("Cannot have negative FOV")
+        else:
+            self.fov = fov
+    
+    """
+    Set Heading
+    heading [rad]
+    The heading of the camera relative to the direction of travel
+    0 = in direction of travel
+    pi/2 = right of direction of travel
+    -pi/2 = left of direction of travel
+    pi = opposite direction of travel
+    """
+    def set_heading(self, heading):
+        self.heading = heading
+    
+    """ 
+    Flush video buffer
+    To get the most recent images, flush the buffer of older frames
+    """
+    def flush_buffer(self, frames):
+        for i in range(frames):
+            (s, bgr) = self.camera.read()
+    
+    """
+    Calculate Speed
+    """
+    def speed(self, samples=2):
+        results = []
+        s1 = False
+        s2 = False
+        for i in range(samples):
+            attempts = 0
+            while not (s1 and s2):
+                t1 = time.time()
+                (s1, bgr1) = self.camera.read()
+                t2 = time.time()
+                (s2, bgr2) = self.camera.read()
+                attempts += 1
+                if attempts > 10:
+                    raise Exception("Camera failure")
+            else:
+                gray1 = cv2.cvtColor(bgr1, cv2.COLOR_BGR2GRAY)
+                gray2 = cv2.cvtColor(bgr2, cv2.COLOR_BGR2GRAY)
+                (pts1, desc1) = self.surf.detectAndCompute(gray1, None)
+                (pts2, desc2) = self.surf.detectAndCompute(gray2, None)
+                if pts1 and pts2:
+                    all_matches = self.matcher.knnMatch(desc1, desc2, k=self.neighbors)
+                    good_matches = []
+                    for m,n in all_matches:
+                        if m.distance < 0.65 * n.distance:
+                            good_matches.append(m)
+                    for match in good_matches:
+                        pt1 = pts1[match.queryIdx]
+                        pt2 = pts2[match.trainIdx]
+                        x1 = int(pt1.pt[0])
+                        y1 = int(pt1.pt[1])
+                        x2 = int(pt2.pt[0])
+                        y2 = int(pt2.pt[1])
+                        distance_px = np.sqrt( (x2 - x1)**2 + (y2 - y1)**2 )
+                        distance_units = distance_px * self.fov / self.frame_w
+                        speed = distance_units / (t2 - t1)
+                        results.append(speed)
+                else:
+                    raise Exception("No matches found: check hessian")
+        return np.mean(results)
+
+if __name__ == '__main__':
+    test = V6()
+    test.speed()
+    test.close()
