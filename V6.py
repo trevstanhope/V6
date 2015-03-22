dd129be921083652f7bf2103927ff695b6fd6366
Trevor Stanhope <trevor@firebase>
2015-03-22 18:01:30 -0400

    first implementation of project()

--- V6.py
+++ V6.py
@@ -5,20 +5,41 @@ import time
 class V6:
     
     """
-    
+    Initialize
     """
-    def __init__(self, capture=0, fov=0.7, dist=100, heading=0, incl=0, hessian=500, frame_w=640, frame_h=480, neighbors=2, factor=0.65):
+    def __init__(self, capture=0, fov=0.7, h=100, roll=0, pitch=0, yaw=0, hessian=500, frame_w=640, frame_h=480, neighbors=2, factor=0.65):
         self.camera = cv2.VideoCapture(capture)
         self.set_matchfactor(factor)
         self.set_resolution(frame_w, frame_h)
-        self.set_fov(fov)
-        self.set_heading(heading)
-        self.set_inclination(incl) # 0 rad
-        self.set_distance(dist) # camera distance at center
+        self.set_fov(fov) # set the field of view (horizontal)
+        self.set_pitch(pitch) # 0 rad
+        self.set_roll(roll) # 0 rad
+        self.set_yaw(yaw) # 0 rad
+        self.set_height(h) # camera distance at center
         self.set_neighbors(neighbors)
         self.surf = cv2.SURF(hessian)
         self.matcher = cv2.BFMatcher()
-    
+
+    """
+    Project points from pixels to metric units
+
+    f : focal length
+    h : height of camera
+    theta_x : angle of inclination
+
+    fov_h = 2 * atan(0.5 * x / f)
+    fov_v = 2 * atan(0.5 * y / f)
+    theta = atan(y / f)
+    Y = h / tan(theta_x - theta)
+    X = x * sqrt( (h^2 + Y^2) / (f^2 + y^2) )
+    """
+    def project(self, x, y):
+        f = self.frame_ / (2 * tan(fov_h / 2.0))
+        theta = np.atan(y / f)
+        Y = self.h / np.tan( (np.pi/2.0 - self.incl) - theta)
+        X = x * np.sqrt( (self.h^2 + Y^2) / (f^2 + y^2) )
+        return (X, Y)
+
     """
     Close
     """  
@@ -58,24 +79,53 @@ class V6:
     Set distance at center of frame
     dist [cm]
     """
-    def set_distance(self, dist):
-        if dist <= 0:
+    def set_height(self, h):
+        if h <= 0:
             raise Exception("Improper distance")
         else:
-            self.dist = dist
+            self.h = h
     
     """
     Set Inclination
     incl [rad]
     0 is orthogonal to surface 
     """
-    def set_inclination(self, incl):
-        if incl < 0:
+    def set_pitch(self, pitch):
+        if pitch < 0:
             raise Exception("Cannot have negative inclination")
-        elif incl > np.pi/2.0:
+        elif pitch > np.pi/2.0:
             raise Exception("Cannot have inclination parallel to surface")
         else:
-            self.incl = incl
+            self.pitch = pitch
+
+    """
+    Set Pitch
+    pitch [rad]
+    0 is orthogonal to surface 
+    """
+    def set_pitch(self, pitch):
+        if pitch < 0:
+            raise Exception("Cannot have negative inclination")
+        elif pitch > np.pi/2.0:
+            raise Exception("Cannot have inclination parallel to surface")
+        else:
+            self.pitch = pitch
+
+    """
+    Set Roll
+    roll [rad]
+    0 is parallel to surface 
+    """
+    def set_roll(self, roll):
+        self.roll = roll
+
+    """
+    Set Yaw (relative to direction of travel)
+    yaw [rad]
+    0 is parallel to direction of travel 
+    """
+    def set_yaw(self, yaw):
+        self.yaw = yaw
     
     """
     Set Field-of-View
@@ -86,18 +136,6 @@ class V6:
             raise Exception("Cannot have negative FOV")
         else:
             self.fov = fov
-    
-    """
-    Set Heading
-    heading [rad]
-    The heading of the camera relative to the direction of travel
-    0 = in direction of travel
-    pi/2 = right of direction of travel
-    -pi/2 = left of direction of travel
-    pi = opposite direction of travel
-    """
-    def set_heading(self, heading):
-        self.heading = heading
     
     """ 
     Flush video buffer
@@ -145,7 +183,7 @@ class V6:
                         y2 = int(pt2.pt[1])
                         if display: cv2.line(output, (x1, y1), (x2 + self.frame_w, y2), (100,0,255), 1)
                         distance_px = np.sqrt( (x2 - x1)**2 + (y2 - y1)**2 ) #! this is going to get complex with heading/incl compensation
-                        distance_units = distance_px * 2 * self.dist * np.tan(self.fov / 2.0) / self.frame_w
+                        distance_units = distance_px * 2 * self.h * np.tan(self.fov / 2.0) / self.frame_w
                         speed = distance_units / (t2 - t1)
                         results.append(speed)
                 else:
